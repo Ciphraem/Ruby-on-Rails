4af93d55342b58ff17cc020ba91a55f852a00c0a
Ciphraem <33486812+Ciphraem@users.noreply.github.com>
2025-06-24 22:54:05 +0800

    Finished studying debugging. Updated .ruby-version

---
+++ script.rb
@@ -0,0 +1,13 @@
+# script.rb => used to practice debugging with VS Code Debugger
+
+def isogram?(string)
+  original_length = string.length
+  string_array = string.downcase.split
+
+  debugger
+
+  unique_length = string_array.uniq.length
+  original_length == unique_length
+end
+
+isogram?("Odin")
